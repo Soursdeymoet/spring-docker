4b392615db8772c89240712208f085733eb7b186
soursdey <soursdey11th@gmail.com>
2025-09-22 22:41:50 +0700

    init project

---
+++ src/test/java/com/docker/my_tutorial/MyTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.my_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
